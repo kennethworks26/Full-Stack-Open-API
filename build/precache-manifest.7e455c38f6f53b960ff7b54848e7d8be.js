2c05843fb661efbfd7a54816b7b11bd5c687b194
Kenneth Fernandez <kenneth@mesasix.com>
2020-05-09 01:30:51 +0800

    Add frontend build, package scripts

---
+++ build/precache-manifest.7e455c38f6f53b960ff7b54848e7d8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a16129ff6a14e3680f02b9407435c33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6c7c5bf441e2d91e35",
+    "url": "/static/css/main.bf4b310b.chunk.css"
+  },
+  {
+    "revision": "7f37b720015ef9748f70",
+    "url": "/static/js/2.0b618ee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b618ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6c7c5bf441e2d91e35",
+    "url": "/static/js/main.696b7955.chunk.js"
+  },
+  {
+    "revision": "4ee863eec7039f899cc1",
+    "url": "/static/js/runtime-main.64cf2cba.js"
+  }
+]);
