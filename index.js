4d7096dec89447a304531c84da4ad22cc7d521fd
Kenneth Fernandez <kenneth@mesasix.com>
2020-05-08 23:25:08 +0800

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -5,27 +5,26 @@ app.use(express.json());
 
 let persons = [
   {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
   },
   {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
   }
 ];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (req, res) => {
+  res.send(
+    `<h1>Phonebook has info for ${persons.length} people</h1>
+    <p>${new Date()}</p>`
+  );
 });
 
 app.get("/api/persons", (req, res) => {
