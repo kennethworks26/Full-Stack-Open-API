51da1716bc92ddd177f1f5e271ac15c241eca504
Kenneth Fernandez <kennethworks26@gmail.com>
2020-05-14 11:14:29 +0800

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,18 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let initialValue = 0;
+
+  let sum = blogs.reduce(
+    (accumulator, currentvalue) => accumulator + currentvalue.likes,
+    initialValue
+  );
+
+  return sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
